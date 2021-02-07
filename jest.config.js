b9615cf22f3b8240f5c28b8d34bc68b4a9ad334b
Scott Brassfield <scott-brassfield@pluralsight.com>
2021-02-07 14:53:36 -0700

    Update jest config to expand coverage collection files

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   collectCoverage: true,
+  collectCoverageFrom: ["<rootDir>/src/*.ts"],
   coveragePathIgnorePatterns: ["/node_modules/", "<rootDir>/src/test/"],
 }
